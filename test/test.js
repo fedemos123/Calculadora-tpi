887a0a502edf139f837efe7753761720888d3da8
fedemos <fedemos123@gmail.com>
2021-06-28 11:56:25 -0300

    fixed test

--- test/test.js
+++ test/test.js
@@ -1,14 +1,14 @@
 'use strict';
 
 var expect = require("chai").expect;
-var calculator = require("../app/calculator");
+var calcu = require("../app/calcu");
 
 describe("Suma - Test", function() {
 
   describe("Testeando", function() {
 
     it("Testeando suma", function() {
-      expect(calculator.suma(1,1)).to.equal(1+1);
+      expect(calcu.suma(1,1)).to.equal(1+1);
     });
 
   });
