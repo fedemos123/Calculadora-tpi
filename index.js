6c7c5abdf5a111fff9de71916d86ed3eece4d6be
fedemos <fedemos123@gmail.com>
2021-06-28 09:11:58 -0300

    prueba hola mundo

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 'use strict';
 
-var calculator = require('./app/calculator');
-
-var a = 3;
-var b = 5;
-
-var v1 = `Add ${a} + ${b} = ${calculator.add(a,b)}`;
-var v2 = `Minus ${a} + ${b} = ${calculator.minus(a,b)}`;
-var v3 = `Multiply ${a} + ${b} = ${calculator.multiply(a,b)}`;
-var v4 = `Divide ${a} + ${b} = ${calculator.divide(a,b)}`;
+var a = 'hola mundo!';
 
 const express = require('express')
 
@@ -16,6 +8,6 @@ const app = express()
 
 const port = process.env.PORT || 8000
 
-app.get('/', (req,res) => res.send("hola"+v1+ "<br/>" +v2+ "<br/>"+v3 +"<br/>"+v4))
+app.get('/', (req,res) => res.send(a))
 
 app.listen(port, () => console.log(port))
---
+++ test/test.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var expect = require("chai").expect;
+var a = require("../index");
+
+expect(a).to.be.a('hola mundo!');
